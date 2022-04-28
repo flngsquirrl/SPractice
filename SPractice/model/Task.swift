597e44b3d9d61dd10a52c687ca59869bd28d16f4
Yuliya Charniak <y.charniak@gmail.com>
2022-04-28 16:05:33 +0400

    Task examples are corrected

--- SPractice/model/Task.swift
+++ SPractice/model/Task.swift
@@ -18,8 +18,8 @@ struct Task {
     static let restTabataWarmUp = Task(type: .rest, name: "warm-up", duration: 10)
     static let activityTabata1 = Task(type: .activity, name: "activity 1", duration: 20)
     static let restTabata1 = Task(type: .rest, name: "rest 1", duration: 10)
-    static let activityTabata2 = Task(type: .activity, name: "activity 1", duration: 20)
-    static let restTabata2 = Task(type: .rest, name: "rest", duration: 10)
+    static let activityTabata2 = Task(type: .activity, name: "activity 2", duration: 20)
+    static let restTabata2 = Task(type: .rest, name: "rest 2", duration: 10)
     static let restTabataCoolDown = Task(type: .rest, name: "cool-down", duration: 10)
     
     static let restService10 = Task(type: .rest, name: "rest", duration: 10)
