c2842ffe938d6155c71a8dab1bb16b4126eb0a94
Yuliya Charniak <y.charniak@gmail.com>
2022-04-29 12:51:14 +0400

    PracticeView is added

---
+++ SPractice/PracticeView.swift
@@ -0,0 +1,24 @@
+//
+//  PracticeView.swift
+//  SPractice
+//
+//  Created by Yuliya Charniak on 29.04.22.
+//
+
+import SwiftUI
+
+struct PracticeView: View {
+    var body: some View {
+        ExcerciseView(excercise: Excercise.catCow)
+    }
+}
+
+struct PracticeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color.orange
+                .ignoresSafeArea()
+            PracticeView()
+        }
+    }
+}
