e2797d46935fde1404e9c0148fddef40ec7b440d
Yuliya Charniak <y.charniak@gmail.com>
2022-08-06 17:03:46 +0200

    [trivial] indentation is fixed

--- SPractice/view/exercises/ExercisesView.swift
+++ SPractice/view/exercises/ExercisesView.swift
@@ -10,100 +10,100 @@ import SwiftUI
 struct ExercisesView: View, MainListView {
     
     typealias Element = ExerciseTemplate
-   
+    
     @ObservedObject var dataManager = Exercises.shared
     @ObservedObject var selectionManager = ExerciseSelectionManager.shared
     @Environment(\.horizontalSizeClass) var sizeClass
-   
+    
     @State internal var searchText = ""
     @AppStorage("exercisesSortProperty") internal var sortProperty: SortProperty = .date
     @AppStorage("exercisesSortOrder") internal var sortOrder: SortOrder = .desc
-   
+    
     @State private var showDeleteConfirmation = false
     @State private var selectedToDelete: ExerciseTemplate?
-   
+    
     @State private var showAddNewView = false
-   
+    
     var body: some View {
-       NavigationView {
-           list
-           .navigationTitle("Exercises")
-           .sheet(isPresented: $showAddNewView) {
-               addItemView
-           }
-           .toolbar {
-               listToolbar
-           }
-           
-           secondaryView
-       }
-       .accentColor(.customAccentColor)
+        NavigationView {
+            list
+                .navigationTitle("Exercises")
+                .sheet(isPresented: $showAddNewView) {
+                    addItemView
+                }
+                .toolbar {
+                    listToolbar
+                }
+            
+            secondaryView
+        }
+        .accentColor(.customAccentColor)
     }
-   
+    
     var list: some View {
-       ScrollViewReader { proxy in
-           List {
-               ForEach(sortedElements) { exercise in
-                   HStack {
-                       NavigationLink(tag: exercise.id, selection: $selectionManager.selection) {
-                           ExerciseDetailsView(for: exercise) {
-                               dataManager.update($0)
-                           } onDelete: { deleteItem($0) }
-                       } label: {
-                           let isAccented = exercise.id == selectedToDelete?.id
-                           ExerciseShortDecorativeView(for: exercise, isIconAccented: isAccented, isNameAccented: isAccented, isFilled: true)
-                       }
-                   }
-               }
-               .onDelete { indexSet in
-                   showDeleteConfirmation = true
-                   selectedToDelete = getSortedElement(index: indexSet.first!)
-               }
-           }
-           .listStyle(.inset)
-           .onChange(of: showDeleteConfirmation) { shouldShow in
-               if !shouldShow {
-                   withAnimation {
-                       selectedToDelete = nil
-                   }
-               }
-           }
-           .searchable(text: $searchText)
-           .disableAutocorrection(true)
-           .onChange(of: selectionManager.newItem) { _ in
-               if selectionManager.newItem != nil {
-                   withAnimation {
-                       proxy.scrollTo(selectionManager.newItem!, anchor: .center)
-                   }
-               }
-           }
-           .alert(DeleteAlertConstants.title, isPresented: $showDeleteConfirmation, presenting: selectedToDelete) { item in
-               DeleteAlertContent(item: item) {
-                   deleteItem($0)
-               }
-           } message: { _ in
-               DeleteAlertConstants.messageText
-           }
-       }
+        ScrollViewReader { proxy in
+            List {
+                ForEach(sortedElements) { exercise in
+                    HStack {
+                        NavigationLink(tag: exercise.id, selection: $selectionManager.selection) {
+                            ExerciseDetailsView(for: exercise) {
+                                dataManager.update($0)
+                            } onDelete: { deleteItem($0) }
+                        } label: {
+                            let isAccented = exercise.id == selectedToDelete?.id
+                            ExerciseShortDecorativeView(for: exercise, isIconAccented: isAccented, isNameAccented: isAccented, isFilled: true)
+                        }
+                    }
+                }
+                .onDelete { indexSet in
+                    showDeleteConfirmation = true
+                    selectedToDelete = getSortedElement(index: indexSet.first!)
+                }
+            }
+            .listStyle(.inset)
+            .onChange(of: showDeleteConfirmation) { shouldShow in
+                if !shouldShow {
+                    withAnimation {
+                        selectedToDelete = nil
+                    }
+                }
+            }
+            .searchable(text: $searchText)
+            .disableAutocorrection(true)
+            .onChange(of: selectionManager.newItem) { _ in
+                if selectionManager.newItem != nil {
+                    withAnimation {
+                        proxy.scrollTo(selectionManager.newItem!, anchor: .center)
+                    }
+                }
+            }
+            .alert(DeleteAlertConstants.title, isPresented: $showDeleteConfirmation, presenting: selectedToDelete) { item in
+                DeleteAlertContent(item: item) {
+                    deleteItem($0)
+                }
+            } message: { _ in
+                DeleteAlertConstants.messageText
+            }
+        }
     }
-   
+    
     var addItemView: some View {
-       AddExerciseView() {
-           addItem($0)
-       }
+        AddExerciseView() {
+            addItem($0)
+        }
     }
-   
+    
     var listToolbar: some ToolbarContent {
-       ToolbarItemGroup(placement: .navigationBarTrailing) {
-           Button() {
-               showAddNewView = true
-           } label: {
-               Image(systemName: "plus")
-                   .frame(width: 25)
-           }
-           
-           SortingMenu(sortProperty: $sortProperty, sortOrder: $sortOrder)
-       }
+        ToolbarItemGroup(placement: .navigationBarTrailing) {
+            Button() {
+                showAddNewView = true
+            } label: {
+                Image(systemName: "plus")
+                    .frame(width: 25)
+            }
+            
+            SortingMenu(sortProperty: $sortProperty, sortOrder: $sortOrder)
+        }
     }
 }
 
--- SPractice/view/programs/ProgramsView.swift
+++ SPractice/view/programs/ProgramsView.swift
@@ -14,95 +14,95 @@ struct ProgramsView: View, MainListView {
     @ObservedObject var dataManager = Programs.shared
     @ObservedObject var selectionManager = ProgramSelectionManager.shared
     @Environment(\.horizontalSizeClass) var sizeClass
-   
+    
     @State internal var searchText = ""
     @AppStorage("programsSortProperty") internal var sortProperty: SortProperty = .date
     @AppStorage("programsSortOrder") internal var sortOrder: SortOrder = .desc
-   
+    
     @State private var showDeleteConfirmation = false
     @State private var selectedToDelete: ProgramTemplate?
-   
+    
     @State private var showAddNewView = false
-   
+    
     var body: some View {
-       NavigationView {
-           list
-           .navigationTitle("Programs")
-           .sheet(isPresented: $showAddNewView) {
-               addItemView
-           }
-           .toolbar {
-               listToolbar
-           }
-           
-           secondaryView
-       }
-       .accentColor(.customAccentColor)
+        NavigationView {
+            list
+                .navigationTitle("Programs")
+                .sheet(isPresented: $showAddNewView) {
+                    addItemView
+                }
+                .toolbar {
+                    listToolbar
+                }
+            
+            secondaryView
+        }
+        .accentColor(.customAccentColor)
     }
-   
+    
     var list: some View {
-       ScrollViewReader { proxy in
-           List {
-               ForEach(sortedElements) { program in
-                   HStack {
-                       NavigationLink(tag: program.id, selection: $selectionManager.selection) {
-                           ProgramDetailsView(for: program) {
-                               dataManager.update($0)
-                           } onDelete: {
-                               deleteItem($0)
-                           }
-                       } label: {
-                           ProgramShortDecorativeView(for: program, isAccented: program.id == selectedToDelete?.id, accentColor: .customAccentColor)
-                       }
-                   }
-               }
-               .onDelete { indexSet in
-                   showDeleteConfirmation = true
-                   selectedToDelete = getSortedElement(index: indexSet.first!)
-               }
-           }
-           .listStyle(.inset)
-           .onChange(of: showDeleteConfirmation) { shouldShow in
-               if !shouldShow {
-                   withAnimation {
-                       selectedToDelete = nil
-                   }
-               }
-           }
-           .searchable(text: $searchText)
-           .disableAutocorrection(true)
-           .onChange(of: selectionManager.newItem) { _ in
-               if selectionManager.newItem != nil {
-                   withAnimation {
-                       proxy.scrollTo(selectionManager.newItem!, anchor: .center)
-                   }
-               }
-           }
-           .alert(DeleteAlertConstants.title, isPresented: $showDeleteConfirmation, presenting: selectedToDelete) { item in
-               DeleteAlertContent(item: item) {
-                   deleteItem($0)
-               }
-           } message: { _ in
-               DeleteAlertConstants.messageText
-           }
-       }
+        ScrollViewReader { proxy in
+            List {
+                ForEach(sortedElements) { program in
+                    HStack {
+                        NavigationLink(tag: program.id, selection: $selectionManager.selection) {
+                            ProgramDetailsView(for: program) {
+                                dataManager.update($0)
+                            } onDelete: {
+                                deleteItem($0)
+                            }
+                        } label: {
+                            ProgramShortDecorativeView(for: program, isAccented: program.id == selectedToDelete?.id, accentColor: .customAccentColor)
+                        }
+                    }
+                }
+                .onDelete { indexSet in
+                    showDeleteConfirmation = true
+                    selectedToDelete = getSortedElement(index: indexSet.first!)
+                }
+            }
+            .listStyle(.inset)
+            .onChange(of: showDeleteConfirmation) { shouldShow in
+                if !shouldShow {
+                    withAnimation {
+                        selectedToDelete = nil
+                    }
+                }
+            }
+            .searchable(text: $searchText)
+            .disableAutocorrection(true)
+            .onChange(of: selectionManager.newItem) { _ in
+                if selectionManager.newItem != nil {
+                    withAnimation {
+                        proxy.scrollTo(selectionManager.newItem!, anchor: .center)
+                    }
+                }
+            }
+            .alert(DeleteAlertConstants.title, isPresented: $showDeleteConfirmation, presenting: selectedToDelete) { item in
+                DeleteAlertContent(item: item) {
+                    deleteItem($0)
+                }
+            } message: { _ in
+                DeleteAlertConstants.messageText
+            }
+        }
     }
-   
+    
     var addItemView: some View {
-       AddProgramView() {
-           addItem($0)
-       }
+        AddProgramView() {
+            addItem($0)
+        }
     }
-   
+    
     var listToolbar: some ToolbarContent {
         ToolbarItemGroup(placement: .navigationBarTrailing) {
             Button() {
-            showAddNewView = true
+                showAddNewView = true
             } label: {
-            Image(systemName: "plus")
-               .frame(width: 25)
+                Image(systemName: "plus")
+                    .frame(width: 25)
             }
-
+            
             SortingMenu(sortProperty: $sortProperty, sortOrder: $sortOrder)
         }
     }
