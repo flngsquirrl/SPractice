ce80cacd30327e67354f3bdde1b81524b4b4febf
flngsquirrl <y.charniak@gmail.com>
2022-09-08 16:21:44 +0200

    Change stub view to contain description text

--- SPractice/view/common/StubView.swift
+++ SPractice/view/common/StubView.swift
@@ -9,15 +9,12 @@ import SwiftUI
 
 struct StabView: View {
     var body: some View {
-        GeometryReader { geo in
-            ZStack {
-                Color(uiColor: .secondarySystemBackground)
-                VStack {
-                    let size = LayoutUtils.getPreferredLogoSize(parentContainerSize: geo.size)
-                    LayoutUtils.getLogo()
-                        .frame(width: size, height: size)
-                }
-            }
+        ZStack {
+            Color(uiColor: .secondarySystemBackground)
+                .ignoresSafeArea()
+            Text("Please, select an item from the left-hand menu.")
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
         }
     }
 }
