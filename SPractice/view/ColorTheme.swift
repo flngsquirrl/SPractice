f85eccf810559c85f7fc67c90b7d692eea13e5da
flngsquirrl <y.charniak@gmail.com>
2022-09-09 21:20:01 +0200

    Delete unused colors

--- SPractice/view/ColorTheme.swift
+++ SPractice/view/ColorTheme.swift
@@ -9,21 +9,9 @@ import Foundation
 import SwiftUI
 
 extension ShapeStyle where Self == Color {
-    static var darkNavy: Color {
-        Color(red: 17/255, green: 43/255, blue: 60/255)
-    }
-
-    static var lightNavy: Color {
-        Color(red: 28/255, green: 80/255, blue: 112/255)
-            .opacity(0.8)
-    }
 
     static var lightOrange: Color {
         Color(red: 216/255, green: 153/255, blue: 91/255)
-    }
-
-    static var darkOrange: Color {
-        Color(red: 224/255, green: 77/255, blue: 1/255)
     }
 
     static var creamy: Color {
