082cb85cdbb4f1f6fb49ad8bd16ec39ad4c23ca4
flngsquirrl <y.charniak@gmail.com>
2022-09-11 20:42:20 +0200

    Delete outdated object dependency

--- SPractice/view/program/ProgramShortDecorativeView.swift
+++ SPractice/view/program/ProgramShortDecorativeView.swift
@@ -13,8 +13,6 @@ struct ProgramShortDecorativeView: View {
     private var isAccented: Bool
     private var accentColor: Color
 
-    @ObservedObject var settings = SettingsManager.settings
-
     init(for program: ProgramTemplate, isAccented: Bool = false, accentColor: Color = .customAccentColor) {
         self.program = program
         self.isAccented = isAccented
