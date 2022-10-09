8be9fe50cad031e02fafc02e6a9c1b63853a79ea
flngsquirrl <y.charniak@gmail.com>
2022-10-09 23:07:51 +0200

    Move controller code from MainManager

--- SPractice/model/common/MainController.swift
+++ SPractice/model/common/MainController.swift
@@ -7,13 +7,51 @@
 
 import Foundation
 
-@MainActor protocol MainController: ObservableObject, ResetableDataManager, SortableFilterableList, SortingSettingsManager {
+@MainActor protocol MainController: ObservableObject, ResetableDataManager, SortableFilterableList, SortingSettingsManager
+    where Item: HavingID {
+
     var newItem: UUID? {get set}
     var selected: Item? {get set}
     var searchText: String {get}
+
+    func onAdd(_ item: Item)
+    func onUpdate(_ item: Item)
+    func onDelete(_ item: Item)
 }
 
 extension MainController {
+
+    func onAdd(_ item: Item) {
+        add(item)
+        applySorting()
+        newItem = item.id
+    }
+
+    func onUpdate(_ item: Item) {
+        update(item)
+        updateSelected(item)
+    }
+
+    func onDelete(_ item: Item) {
+        delete(item)
+        resetSelected(item)
+    }
+
+    private func updateSelected(_ item: Item) {
+        if let selected {
+            if item.id == selected.id {
+                self.selected = item
+            }
+        }
+    }
+
+    private func resetSelected(_ item: Item) {
+        if let selected {
+            if item.id == selected.id {
+                self.selected = nil
+            }
+        }
+    }
 
     var filteredItems: [Item] {
         filter(by: searchText)
