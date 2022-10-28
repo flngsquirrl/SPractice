e67d83bda71a538f0c8a425c153b6ccdf4f5fdc3
flngsquirrl <y.charniak@gmail.com>
2022-10-28 19:40:35 +0200

    Remove unused code

--- SPractice/model/exercise/ExerciseType.swift
+++ SPractice/model/exercise/ExerciseType.swift
@@ -8,11 +8,9 @@
 import Foundation
 
 enum ExerciseType: String, CaseIterable, Codable {
+
     case flow
     case timer
     case tabata
 
-    var hasDuration: Bool {
-        self != .flow
-    }
 }
